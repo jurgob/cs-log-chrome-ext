ed1186508a791465b8a05313a42b7bee6e586170
Jurgo Boemo <jurgo.boemo@gmail.com>
2019-06-14 00:56:03 +0100

    improve error debug

--- popup.js
+++ popup.js
@@ -8,12 +8,31 @@ chrome.storage.sync.get('color', function(data) {
 changeColor.onclick = function(element) {
   let color = element.target.value;
   const script = `
+  function levelStr(lev){
+    if(lev === 30)
+      return "INFO"
+    else if(lev === 40)
+      return "WARN"
+    else if (lev === 50)
+      return "ERROR"
+  }
+
+  let colors = {
+      "INFO": "#149DA0",
+      "WARN": "#D18BE1",
+      "ERROR": "#860303",
+      "DEBUG": "#B2B200"
+  }
+
   var logLinesEls = document.querySelectorAll('dl.truncate-by-height')
   var logLines = [].slice.call(logLinesEls).map(el => el.children[1].innerText).map(j => JSON.parse(j))
   logLinesEls.forEach((el, idx) => {
     let parentEl = el.parentElement.parentElement
     parentEl.className ="";
-  	parentEl.innerHTML = "<div></div><pre>"+JSON.stringify(logLines[idx], '  ', '  ')+"<pre>";
+    let logLine = logLines[idx]
+    const levelString = levelStr(logLine.level)
+    const firstEl ="<div style='color:"+colors[levelString]+"'>"+levelString+": "+logLine.msg+"</div>";
+  	parentEl.innerHTML = firstEl + "<pre>"+JSON.stringify(logLine, '  ', '  ')+"<pre>";
   })
 `
 
